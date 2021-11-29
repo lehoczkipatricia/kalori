9ac014de95fa8fbc3d6fdb10b2e9c60769ace854
Lehoczki Patricia <pati@gmail.com>
2021-11-29 08:37:41 +0100

    függvények megírása, mehjegyzések írása, test mappa létrehozása

---
+++ src/App.java
@@ -0,0 +1,12 @@
+// Név: Lehoczki Patrícia
+// Dátum: 2021.11.29
+
+public class App {
+    public double calcWoman(double weight, double height, int age){
+        return 9.247*weight+3.098*height-4.330*age+447.593;
+    }
+
+    public double calcMan(double weight, double height, int age){
+        return 13.397*weight+4.799*height-5.667*age+88.362;
+    }
+}
